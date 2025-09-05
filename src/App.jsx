72258f83b263e811fd14c0297aae90ea130681ad
aldikasa <eraldkasaj14@gmail.com>
2025-09-05 22:25:15 +0200

    Initial commit - Cryptoverse Academy

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+     <div>
+      <h1>Cryptoverse Academy</h1>
+      <p>Welcome to the crypto learning platform!</p>
+    </div>
+  )
+}
+
+export default App
